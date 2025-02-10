1cfa26876dbe2b844141a610da4ca551cda192af
Kyouma960 <gautamnitesh960@gmail.com>
2025-02-10 11:58:37 +0000

    first

--- utils/transact/validate_transaction.py
+++ utils/transact/validate_transaction.py
@@ -1,4 +1,4 @@
 from utils.ledger import ledger_util
 
-def is_valid(method,adr_x,amount):
-    if(ledger_util.)
+def is_valid(adr_x):
+
--- utils/verify/wallet_verify.py
+++ utils/verify/wallet_verify.py
@@ -1,2 +1,6 @@
+from utils/ledger import ledger_util
 def is_valid(data):
-    return True
+    if(ledger_util.ledger_scan(ledger_util.wallet_path,adr_x)):
+        return True
+    else:
+        return False
